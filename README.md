2483f71ce8e98a21d8338a1f1451eb5f70a990cd
joelpdurham <joeldur@gmail.com>
2019-12-18 13:39:48 -0800

    wrote failing test for create route

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # mongo-aggregations
+
+##Write a seed data script
+Create a script that can seed your database. Use your script to seed your database before your tests.
+
+##Write CRUD routes for a model
+Create a model and all CRUD routes for the model
+
+##Create static methods for aggregations
+Write static methods on your model to run your aggregations
+
+##Create routes to get your aggregations
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Book = require('../lib/models/Book');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -14,7 +15,41 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  // beforeEach(async() => {
+  //   book = await Book.create
+  // });
+
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can create a new book', () => {
+    const date = new Date();
+    return request(app)
+      .post('/api/v1/books')
+      .send({
+        title: 'New Book',
+        author: 'Durham, Joel',
+        myRating: 5,
+        avgRating: 4.2,
+        binding: 'Hardback',
+        pages: 302,
+        publicationYear: date,
+        dateRead: date
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          title: 'New Book',
+          author: 'Durham, Joel',
+          myRating: 5,
+          avgRating: 4.2,
+          binding: 'Hardback',
+          pages: 302,
+          publicationYear: date.toISOString(),
+          dateRead: date.toISOString()
+        });
+      });
   });
 });
